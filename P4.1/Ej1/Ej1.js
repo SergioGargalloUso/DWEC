266f631eb510877200da0ea6cd1920137979e35f
SergioGargalloUso <sergiogargallo9@gmail.com>
2024-10-21 11:41:05 +0200

    1.1

--- P4.1/Ej1/Ej1.js
+++ P4.1/Ej1/Ej1.js
@@ -6,5 +6,35 @@ function factorial(n) {
    
     return n * factorial(n - 1);
 }
+let numeros = [];
+
+function capturarNumero() {
+    const input = document.getElementById('numero').value;
+
+    // Convertimos el valor a número y lo agregamos al array
+    const numero = parseInt(input);
+    if (!isNaN(numero)) {
+        numeros.push(numero);
+    }
+
+    document.getElementById('numero').value = '';
+
+}
 
 
+
+function mostrarResultados() {
+    const resultadosDiv = document.getElementById('resultados');
+    resultadosDiv.innerHTML = ''; // Limpiamos resultados previos
+
+    // Recorrer los números introducidos y calcular el factorial
+    numeros.forEach(numero => {
+        const resultado = `Factorial de ${numero}: ${factorial(numero)}`;
+        const p = document.createElement('p');
+        p.textContent = resultado;
+        resultadosDiv.appendChild(p);
+    });
+
+    // Limpiamos el array de números
+    numeros = [];
+}
